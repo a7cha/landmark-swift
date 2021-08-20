2cdd1a5c18ed651db58706461a09e432b80ef9b7
arungicahaya <arungicahaya8@gmail.com>
2021-08-21 04:48:08 +0700

    Initial Commit

---
+++ Shared/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Shared
+//
+//  Created by Arungi Cahaya on 21/08/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
