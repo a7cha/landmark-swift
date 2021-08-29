24e9a9291684421edcf4c2ed3b3d2bb574af9d15
arungicahaya <arungicahaya8@gmail.com>
2021-08-29 15:12:48 +0700

    finishing Building List navigation

---
+++ Shared/View/LandmarkList.swift
@@ -0,0 +1,30 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Arungi Cahaya on 29/08/21.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView{
+            List(landmarks){ landmark in
+                NavigationLink(destination : LandmarkDetail(landmark : landmark)){
+                    LandmarkRow(landmark: landmark)
+                }
+            }.navigationTitle("Landmark")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id : \.self){
+            deviceName in
+            LandmarkList().previewDevice(PreviewDevice(rawValue: deviceName)).previewDisplayName(deviceName)
+        }
+
+    }
+}
